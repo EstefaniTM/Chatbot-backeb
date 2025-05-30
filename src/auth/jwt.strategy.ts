1367087cb6e597b39b1f1855b61838fac91e39b8
fcosilva@hotmail.com <estefani.tipantunia542@gmail.com>
2025-05-29 20:39:52 -0500

    Fixsecret

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 interface JwtPayload {
   id: string;
@@ -16,11 +17,11 @@ interface ValidatedUser {
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "process.env.JWT_SECRET",
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
